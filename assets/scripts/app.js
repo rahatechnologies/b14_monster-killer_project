1a2aff2cc51ac15b6354e03d52ec907631f79a87
RAHA Technologies <117338820+rahatechnologies@users.noreply.github.com>
2024-03-03 11:51:37 +0530

    Add files via upload

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,12 @@
 const PLAYER_ATTACK_VALUE = 10;
 const PLAYER_STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const PLAYER_HEAL_VALUE = 20;
 
 const ATTACK_MODE = 'ATTACK';
 const STRONG_ATTACK_MODE = 'STRONG_ATTACK';
+const HEAL_MODE = 'HEAL';
+let hasBonusLife = true;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -11,25 +14,26 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(100);
 
-function attackMonster(mode) {
-  let maxDamage;
-  console.log(mode);
 
-  if (mode === ATTACK_MODE) {
-    maxDamage = PLAYER_ATTACK_VALUE;
-  } else if (mode === STRONG_ATTACK_MODE) {
-    maxDamage = PLAYER_STRONG_ATTACK_VALUE;
-  }
-
-  const monsterDamage = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= monsterDamage;
-
+function endRound(monsterDamage) {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   console.log(`==============================================`);
+  console.log(`playerDamage: ${playerDamage}`);
+  console.log(`monsterDamage: ${monsterDamage}`);
   console.log(`currentMonsterHealth:${currentMonsterHealth}`);
   console.log(`currentPlayerHealth:${currentPlayerHealth}`);
   console.log(`==============================================`);
+
+  if(currentPlayerHealth <= 0 && hasBonusLife){
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    console.log("You would be dead but the bonus life saved you!")
+    setPlayerHealth(initialPlayerHealth);
+    console.log(`currentPlayerHealth:${currentPlayerHealth}`);
+  }
 
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     console.log(`You won!`);
@@ -40,6 +44,23 @@ function attackMonster(mode) {
   }
 }
 
+function attackMonster(mode) {
+  let maxDamage;
+  console.log(mode);
+
+  if (mode === ATTACK_MODE) {
+    maxDamage = PLAYER_ATTACK_VALUE;
+  } else if (mode === STRONG_ATTACK_MODE) {
+    maxDamage = PLAYER_STRONG_ATTACK_VALUE;
+  }
+  const monsterDamage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= monsterDamage;
+ 
+  endRound(monsterDamage);
+
+}
+
+
 function attackHandler() {
   attackMonster(ATTACK_MODE);
 }
@@ -48,5 +69,28 @@ function strongAttackHandler() {
   attackMonster(STRONG_ATTACK_MODE);
 }
 
+function healPlayerHandler() {
+  console.log(HEAL_MODE);
+  let updatedHealValue;
+  if (currentPlayerHealth >= chosenMaxLife - PLAYER_HEAL_VALUE) {
+     console.log(`WARNING - You can't heal more than your max initial health i.e. ${chosenMaxLife}`);                
+     updatedHealValue = chosenMaxLife - currentPlayerHealth;
+    
+  } else {
+    updatedHealValue = PLAYER_HEAL_VALUE;
+
+  }
+
+  // UI / HTML progress bar update
+  increasePlayerHealth(updatedHealValue);
+
+  // internal data update
+  currentPlayerHealth += updatedHealValue;
+  
+  //  Attack Player and check winning conditions
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
---
+++ assets/scripts/modalForm.js
@@ -0,0 +1,75 @@
+// Show a half-transparent DIV to "shadow" the page
+// (the form is not inside, but near it, because it shouldn't be half-transparent)
+function showCover() {
+  let coverDiv = document.createElement('div');
+  coverDiv.id = 'cover-div';
+
+  // make the page unscrollable while the modal form is open
+  document.body.style.overflowY = 'hidden';
+
+  document.body.append(coverDiv);
+}
+
+function hideCover() {
+  document.getElementById('cover-div').remove();
+  document.body.style.overflowY = '';
+}
+
+function showPrompt(text, callback) {
+  showCover();
+  let form = document.getElementById('prompt-form');
+  let container = document.getElementById('prompt-form-container');
+  document.getElementById('prompt-message').innerHTML = text;
+  form.text.value = '';
+
+  function complete(value) {
+    hideCover();
+    container.style.display = 'none';
+    document.onkeydown = null;
+    callback(value);
+  }
+
+  form.onsubmit = function () {
+    let value = form.text.value;
+    if (value == '') return false; // ignore empty submit
+
+    complete(value);
+    return false;
+  };
+
+  form.cancel.onclick = function () {
+    complete(null);
+  };
+
+  document.onkeydown = function (e) {
+    if (e.key == 'Escape') {
+      complete(null);
+    }
+  };
+
+  let lastElem = form.elements[form.elements.length - 1];
+  let firstElem = form.elements[0];
+
+  lastElem.onkeydown = function (e) {
+    if (e.key == 'Tab' && !e.shiftKey) {
+      firstElem.focus();
+      return false;
+    }
+  };
+
+  firstElem.onkeydown = function (e) {
+    if (e.key == 'Tab' && e.shiftKey) {
+      lastElem.focus();
+      return false;
+    }
+  };
+
+  container.style.display = 'block';
+  form.elements.text.focus();
+}
+
+document.getElementById('show-button').onclick = function () {
+  showPrompt('Enter something<br>...smart :)', function (value) {
+    alert('You entered: ' + value);
+  });
+};
